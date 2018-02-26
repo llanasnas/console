b0c6131b0d0ca0b63fef262de6ac3774a8d5323d
llanas <llanasnas@gmail.com>
2018-02-26 17:11:23 +0100

    costras

--- console.php
+++ console.php
@@ -10,9 +10,10 @@
 
 
 ?>
-<html>
+<html >
 
 <head>
+
     <title>Console Aplication</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -25,21 +26,23 @@
 
 </head>
 <body>
-
-<div class="container-fluid col-md-4 col-md-offset-4" style="margin-top:5%">
+<div style="background-color: blue;width: 100%;height: 100%;">
+<div class="container-fluid col-md-4 col-md-offset-4" style="margin-top:5%;">
 
 <form method="post" action="executa.php" class="form-group">
 
-    <h2 class="text-center">Introdueix la acció a executar: </h2>
-    <br>
 
+    <br>
     <div class="pagination-centered">
-        <textarea class="form-control" name="command" placeholder="root@gerard-PC~#" id="exampleFormControlTextarea1" rows="7" style=" background-color: black; color: chartreuse"></textarea>
+
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="root@gerard-PC~#" style=" background-color: black; color: chartreuse">
+        <!--<textarea class="form-control" name="command" placeholder="root@gerard-PC~#" id="exampleFormControlTextarea1" rows="1" style=" background-color: black; color: chartreuse"></textarea>-->
     </div>
     <br>
 
     <div><input class="btn btn-primary center center-block"  type="submit" name="submit" value="submit"></div>
 </form>
+</div>
 </div>
 </body>
 </html>
